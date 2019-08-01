0e5d464a17054e26634ecd6180155e41260399c1
Nathan Fowler <luxurysandbox@gmail.com>
2019-08-01 11:44:07 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Breaktime Node Style
+
+A simple timer written in node to remind you to get off the computer and move around a bit.
+
+## How to use
+
+Clone and then move to the directory
+
+Install dependencies with:  ```npm i```
+
+Replace or add the images or gifs of your choice into the assets folder.
+
+To run the app:  ```node .```
+
+Enter in how many minutes you want to have before each break prompt and enjoy.
+
+When work is done and it's time to end the program: ```^c```
+
+
+### Sample Assets Attribution
+
+"GIF Roundup" by Victoria Cossack is licensed under CC BY-NC-ND 4.0 
+
+"POP POP POP" by Sol Ryu is licensed under CC BY-NC-ND 4.0 
+
+"Heart Breakin'" by Jennifer Teichman is licensed under CC BY-NC-ND 4.0 
+
+"GIF Roundup" by Victoria Cossack is licensed under CC BY-NC-ND 4.0 
+
+https://creativecommons.org/licenses/by-nc-nd/4.0/?ref=ccsearch&atype=rich
+
---
+++ breaktime.js
@@ -0,0 +1,59 @@
+// load our dependencies
+const chalk = require('chalk')
+const inquirer = require('inquirer');
+const figlet = require('figlet');
+const open = require('open');
+const fs = require('fs');
+
+// used to store the assets for use
+const assets = [];
+
+// reads the contents of the assets/ directory
+// and pushes the contents to the assets array
+fs.readdirSync('assets/', {withFileTypes: true})
+  .filter(item => !item.isDirectory())
+  .map(item => assets.push(item.name))
+
+// creates the console title text
+figlet.text('Breaktime!', {
+  font: 'Standard',
+  horizontalLayout: 'default',
+  verticalLayout: 'default'
+}, function(err, data) {
+  if (err) {
+    console.log('Well that did not work');
+    return;
+  }
+  console.log(data);
+})
+
+// prompts the user for how long they would like
+// between breaks.  timeout is to sync the prompt
+// with figlet. 
+setTimeout(() => { 
+inquirer
+  .prompt([
+    {
+      type: 'number',
+      message: chalk.magenta('How many minutes between breaks?'),
+      name: 'time',
+    }
+  ])
+  .then(answers => {
+    breakTime(answers.time);
+  });
+}, 100);
+
+// creates a loop to randomly select one of the assets
+// in the assets directory to be displayed by whatever
+// os the user is using
+function breakTime(num) {
+  const delay = 1000 * 60 * num;
+  setTimeout(() => {
+    (async () => {
+      await open(`./assets/${assets[Math.floor(Math.random() * assets.length)]}`, {wait: true});
+      console.log('app has closed');
+      breakTime(num);
+    })();
+  }, delay);
+};
